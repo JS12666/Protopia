84fb56342d589c5cad51bc70b4a5ce9d330df635
JS12666 <wltjd5672@naver.com>
2026-08-19 03:06:56 +0900

    Initial commit

---
+++ src/main/java/study/protopia/ProtopiaApplication.java
@@ -0,0 +1,13 @@
+package study.protopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtopiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtopiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/protopia/ProtopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.protopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtopiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
