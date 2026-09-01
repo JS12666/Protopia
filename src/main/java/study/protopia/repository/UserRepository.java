7511a184d9d6b61c5ebbdc2b0ce978151f6682cb
JS12666 <wltjd5672@naver.com>
2026-09-01 22:29:06 +0900

    UserRepository 추가

---
+++ src/main/java/study/protopia/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package study.protopia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Integer, Integer> {
+
+    // 모든 정보 조회
+
+    // 특정 정보 조회
+
+}
