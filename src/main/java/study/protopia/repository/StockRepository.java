b11cf1b5649cccf8b4cb627a8a1b434cb4251a03
JS12666 <wltjd5672@naver.com>
2026-09-01 22:27:17 +0900

    add Description

--- src/main/java/study/protopia/repository/StockRepository.java
+++ src/main/java/study/protopia/repository/StockRepository.java
@@ -3,4 +3,12 @@ package study.protopia.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StockRepository extends JpaRepository<String, String> {
+
+    // 주식 구매
+
+    // 주식 판매
+
+    // 내주식 조회
+
+    // 실시간 주가 조회
 }
